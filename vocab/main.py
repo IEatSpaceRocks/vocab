89eb17faacb04e03e080a90dc768bb5d06825b4d
IEatSpaceRocks <149251510+IEatSpaceRocks@users.noreply.github.com>
2025-01-19 14:37:22 +0100

    v0.1.1
    Added a way to show which language we are translating to which.

--- vocab/main.py
+++ vocab/main.py
@@ -6,10 +6,14 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 with open('database.txt', 'r') as file:
     db = file.readlines()
 
-seq = list(range(len(db)))
+seq = list(range(len(db) - 1))
+print(seq)
 random.shuffle(seq)
 
+lang1, lang2 = db[0].strip().split(', ')
+print(f'Translating {lang1} to {lang2}.')
+
 for i in range(len(seq)):
-    word1, word2 = db[seq[i]].strip().split(', ')
+    word1, word2 = db[seq[i] + 1].strip().split(', ')
     print(word1, word2)
     
